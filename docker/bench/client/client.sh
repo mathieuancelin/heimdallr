ce35691796378884db1c2152c9963896ed9d4b3e
Mathieu ANCELIN <mathieu.ancelin@gmail.com>
2018-05-31 09:01:57 +0200

    Fix url

--- docker/bench/client/client.sh
+++ docker/bench/client/client.sh
@@ -1,18 +1,16 @@
 echo "Sleeping"
 sleep 20
 echo "Warm up ..."
-wrk -R 1000 -t6 -c200 -d40s -H "Host: test.foo.bar" --latency http://heimdallr:8091/
-# wrk2 -R 1000 -t6 -c200 -d40s -H "Host: test.foo.bar" --latency http://heimdallr:8091/
+wrk -R 2000 -t6 -c200 -d40s -H "Host: test.foo.bar" --latency http://heimdallr:8091/
 echo "Warm up done !"
 sleep 10
 echo "Bench ..."
 wrk -R 8000 -t80 -c800 -d60s -H "Host: test.foo.bar" --latency http://heimdallr:8091/
-# wrk2 -R 8000 -t80 -c800 -d60s -H "Host: test.foo.bar" --latency http://heimdallr:8091/
 
 echo "Testing on graal VM"
 
 echo "Warm up ..."
-wrk -R 1000 -t6 -c200 -d40s -H "Host: test.foo.bar" --latency http://heimdallrgraalvm:8092/
+wrk -R 2000 -t6 -c200 -d40s -H "Host: test.foo.bar" --latency http://heimdallrgraalvm:8092/
 echo "Warm up done !"
 sleep 10
 echo "Bench ..."
